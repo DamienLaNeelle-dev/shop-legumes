d2ff9672e1e678431d7ba7219fa52b905df63668
Damien L <damien.laneelle33@gmail.com>
2025-12-02 14:07:11 +0100

    wip

--- src/app/legume-list/legume-list.component.ts
+++ src/app/legume-list/legume-list.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-legume-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './legume-list.component.html',
-  styleUrl: './legume-list.component.css'
+  styleUrl: './legume-list.component.css',
 })
 export class LegumeListComponent {
+  constructor(private shopService: ShopService) {}
 
+  legumes: any[] = [];
+
+  ngOnInit(): void {
+    this.shopService.getLegumes().subscribe((data) => {
+      this.legumes = data;
+    });
+  }
 }
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CategoryListComponent } from '../category-list/category-list.component';
+import { LegumeListComponent } from '../legume-list/legume-list.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [CategoryListComponent],
+  imports: [CategoryListComponent, LegumeListComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css',
 })
